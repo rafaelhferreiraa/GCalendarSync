89f3175bae51df85cefae328a0e143e4d60efd7d
rafaelhferreiraa <rafaelheferreira@gmail.com>
2025-04-29 20:42:16 -0300

    zerroutudo

--- src/google-auth.js
+++
@@ -1,9 +0,0 @@
-// pages/api/google-auth.js
-export default async function handler(req, res) {
-    const { code } = req.query;
-    // Aqui você vai usar o 'code' para pegar o token de acesso do Google
-    // Esse processo vai envolver o uso de OAuth do Google, criando um servidor de autorização
-    // Implementar a troca do código de autorização por um token de acesso
-    res.status(200).json({ message: 'Autenticado com o Google' });
-  }
-  
